ed48750106e57b03b007ff957156dc57750403a8
Abhishek Kumar <abhikavi@Abhisheks-MacBook-Pro.local>
2024-10-27 23:07:06 +0530

    love pics

--- src/routes/love/+page.svelte
+++ src/routes/love/+page.svelte
@@ -1,23 +1,33 @@
+<script>
+import a from '$lib/images/a.jpg';
+import b from '$lib/images/b.jpg';
+import c from '$lib/images/c.jpg';
+import d from '$lib/images/d.jpg';
+import e from '$lib/images/e.jpg';
+
+
+
+</script>
 <div class="grid grid-cols-2 gap-4 md:grid-cols-4">
     <div class="grid gap-4">
       <div>
         <img
           class="h-auto max-w-full rounded-lg object-cover object-center"
-          src="https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1950&amp;q=80"
+          src={a}
           alt="gallery-photo"
         />
       </div>
       <div>
         <img
           class="h-auto max-w-full rounded-lg object-cover object-center "
-          src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80"
+          src={b}
           alt="gallery-photo"
         />
       </div>
       <div>
         <img
           class="h-auto max-w-full rounded-lg object-cover object-center"
-          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
+          src={c}
           alt="gallery-photo"
         />
       </div>
@@ -26,14 +36,30 @@
       <div>
         <img
           class="h-auto max-w-full rounded-lg object-cover object-center"
-          src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
+          src={d}
           alt="gallery-photo"
         />
       </div>
       <div>
         <img
           class="h-auto max-w-full rounded-lg object-cover object-center"
-          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+          src={e}
+          alt="gallery-photo"
+        />
+      </div>
+      <!-- <div>
+        <img
+          class="h-auto max-w-full rounded-lg object-cover object-center "
+          src="https://docs.material-tailwind.com/img/team-3.jpg"
+          alt="gallery-photo"
+        />
+      </div>
+    </div>
+    <div class="grid gap-4">
+      <div>
+        <img
+          class="h-auto max-w-full rounded-lg object-cover object-center"
+          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
           alt="gallery-photo"
         />
       </div>
@@ -44,22 +70,6 @@
           alt="gallery-photo"
         />
       </div>
-    </div>
-    <div class="grid gap-4">
-      <div>
-        <img
-          class="h-auto max-w-full rounded-lg object-cover object-center"
-          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
-          alt="gallery-photo"
-        />
-      </div>
-      <div>
-        <img
-          class="h-auto max-w-full rounded-lg object-cover object-center "
-          src="https://docs.material-tailwind.com/img/team-3.jpg"
-          alt="gallery-photo"
-        />
-      </div>
       <div>
         <img
           class="h-auto max-w-full rounded-lg object-cover object-center"
@@ -79,10 +89,10 @@
       <div>
         <img
           class="h-auto max-w-full rounded-lg object-cover object-center"
-          src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80"
+          src="$lib/images/IMG_20180831_222302386_LL~2.jpg"
           alt="gallery-photo"
         />
-      </div>
+      </div> -->
     </div>
   </div>
   
