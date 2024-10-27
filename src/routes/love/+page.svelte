ae69ef2de7f4e2f72628adc986cb6f361b9bfc3c
Abhishek Kumar <abhikavi@Abhisheks-MacBook-Pro.local>
2024-10-27 23:28:00 +0530

    love pics

--- src/routes/love/+page.svelte
+++ src/routes/love/+page.svelte
@@ -4,6 +4,10 @@ import b from '$lib/images/b.jpg';
 import c from '$lib/images/c.jpg';
 import d from '$lib/images/d.jpg';
 import e from '$lib/images/e.jpg';
+import f from '$lib/images/f.jpg';
+import g from '$lib/images/g.jpg';
+import h from '$lib/images/h.jpg';
+import i from '$lib/images/i.jpg';
 
 
 
@@ -47,52 +51,38 @@ import e from '$lib/images/e.jpg';
           alt="gallery-photo"
         />
       </div>
-      <!-- <div>
+      <div>
         <img
           class="h-auto max-w-full rounded-lg object-cover object-center "
-          src="https://docs.material-tailwind.com/img/team-3.jpg"
+          src={f}
           alt="gallery-photo"
         />
       </div>
     </div>
     <div class="grid gap-4">
-      <div>
-        <img
-          class="h-auto max-w-full rounded-lg object-cover object-center"
-          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
-          alt="gallery-photo"
-        />
-      </div>
-      <div>
-        <img
-          class="h-auto max-w-full rounded-lg object-cover object-center "
-          src="https://docs.material-tailwind.com/img/team-3.jpg"
-          alt="gallery-photo"
-        />
-      </div>
-      <div>
-        <img
-          class="h-auto max-w-full rounded-lg object-cover object-center"
-          src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
-          alt="gallery-photo"
-        />
-      </div>
-    </div>
-    <div class="grid gap-4">
-      <div>
-        <img
-          class="h-auto max-w-full rounded-lg object-cover object-center"
-          src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
-          alt="gallery-photo"
-        />
+        <div>
+          <img
+            class="h-auto max-w-full rounded-lg object-cover object-center"
+            src={i}
+            alt="gallery-photo"
+          />
+        </div>
+        <div>
+          <img
+            class="h-auto max-w-full rounded-lg object-cover object-center "
+            src={h}
+            alt="gallery-photo"
+          />
+        </div>
+          
       </div>
       <div>
         <img
           class="h-auto max-w-full rounded-lg object-cover object-center"
-          src="$lib/images/IMG_20180831_222302386_LL~2.jpg"
+          src={g}
           alt="gallery-photo"
         />
-      </div> -->
-    </div>
+      </div>
+    
   </div>
   
