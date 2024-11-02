1aab9091c5a49e19da0d13e2be46ef0be6e700bc
Abhishek Kumar <abhikavi@Abhisheks-MacBook-Pro.local>
2024-11-02 23:35:52 +0530

    initial landing page

--- src/lib/components/layout/features.svelte
+++ src/lib/components/layout/features.svelte
@@ -1,41 +1,41 @@
 <script>
 const features = [
   {
-    title: "Bring Your Own Framework",
+    title: "Validate your website online free",
     description:
-      "Build your site using React, Svelte, Vue, Preact, web components, or just plain ol' HTML + JavaScript.",
+      "Scan your website for any cybersecurity vunrebalities",
     icon: "bx:bxs-briefcase",
   },
-  {
-    title: "100% Static HTML, No JS",
-    description:
-      "Astro renders your entire page to static HTML, removing all JavaScript from your final build by default.",
-    icon: "bx:bxs-window-alt",
-  },
-  {
-    title: "On-Demand Components",
-    description:
-      "Need some JS? Astro can automatically hydrate interactive components when they become visible on the page.  ",
-    icon: "bx:bxs-data",
-  },
-  {
-    title: "Broad Integration",
-    description:
-      "Astro supports TypeScript, Scoped CSS, CSS Modules, Sass, Tailwind, Markdown, MDX, and any other npm packages.",
-    icon: "bx:bxs-bot",
-  },
-  {
-    title: "SEO Enabled",
-    description:
-      "Automatic sitemaps, RSS feeds, pagination and collections take the pain out of SEO and syndication. It just works!",
-    icon: "bx:bxs-file-find",
-  },
-  {
-    title: "Community",
-    description:
-      "Astro is an open source project powered by hundreds of contributors making thousands of individual contributions.",
-    icon: "bx:bxs-user",
-  },
+//   {
+//     title: "100% Static HTML, No JS",
+//     description:
+//       "Astro renders your entire page to static HTML, removing all JavaScript from your final build by default.",
+//     icon: "bx:bxs-window-alt",
+//   },
+//   {
+//     title: "On-Demand Components",
+//     description:
+//       "Need some JS? Astro can automatically hydrate interactive components when they become visible on the page.  ",
+//     icon: "bx:bxs-data",
+//   },
+//   {
+//     title: "Broad Integration",
+//     description:
+//       "Astro supports TypeScript, Scoped CSS, CSS Modules, Sass, Tailwind, Markdown, MDX, and any other npm packages.",
+//     icon: "bx:bxs-bot",
+//   },
+//   {
+//     title: "SEO Enabled",
+//     description:
+//       "Automatic sitemaps, RSS feeds, pagination and collections take the pain out of SEO and syndication. It just works!",
+//     icon: "bx:bxs-file-find",
+//   },
+//   {
+//     title: "Community",
+//     description:
+//       "Astro is an open source project powered by hundreds of contributors making thousands of individual contributions.",
+//     icon: "bx:bxs-user",
+//   },
 ];
 </script>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-	import Features from '$lib/components/layout/features.svelte';
+	import Container from '$lib/components/layout/container.svelte';
+import Features from '$lib/components/layout/features.svelte';
 import Button from '$lib/components/ui/button/button.svelte';
 </script>
-
-<main class="grid place-items-center pb-8 pt-16 md:pb-24 md:pt-12 lg:grid-cols-2">
+<Container><main class="grid place-items-center pb-8 pt-16 md:pb-24 md:pt-12 lg:grid-cols-2">
 	<div class="hidden py-6 md:order-1 md:block"></div>
 	<div>
 		<h1 class="text-5xl font-bold lg:text-6xl lg:tracking-tight xl:text-7xl xl:tracking-tighter">
@@ -11,7 +11,7 @@ import Button from '$lib/components/ui/button/button.svelte';
 		</h1>
 		<p class="mt-4 max-w-xl text-lg text-slate-600">
 			Nutbolt is a starter for someone who want to understand cybersecurity.<wbr /> Let us know
-			if you want a help in cybersecurity front. We have lots of tools and solution for you.
+			if you want a help in cybersecurity front. We have tools and solutions for you to keep your websites secure.
 		</p>
 		<div class="mt-6 flex flex-col gap-3 sm:flex-row">
 			<Button
@@ -35,9 +35,9 @@ import Button from '$lib/components/ui/button/button.svelte';
 	</div>
 </main>
 
-<Features></Features>
+<!-- <Features></Features> -->
 
-
+<!-- 
 <div class="mt-24">
   <h2 class="text-center text-slate-500">Works with your technologies</h2>
   <div class="flex gap-8 md:gap-20 items-center justify-center mt-10 flex-wrap">
@@ -45,19 +45,20 @@ import Button from '$lib/components/ui/button/button.svelte';
 Icons
 ICONS
   </div>
-</div>
+</div> -->
 
 
 <div
   class="bg-black p-8 md:px-20 md:py-20 mt-20 mx-auto max-w-5xl rounded-lg flex flex-col items-center text-center">
   <h2 class="text-white text-4xl md:text-6xl tracking-tight">
-    Build faster websites.
+    Scan your websites.
   </h2>
   <p class="text-slate-400 mt-4 text-lg md:text-xl">
-    Pull content from anywhere and serve it fast with Astro's next-gen island
-    architecture.
+    Scan your website for any cyber vulnerabilities.
   </p>
   <div class="flex mt-5">
-    <Button href="#" style="inverted">Get Started</Button>
+    <Button href="/security-scan" style="inverted">Get Started</Button>
   </div>
 </div>
+</Container>
+
